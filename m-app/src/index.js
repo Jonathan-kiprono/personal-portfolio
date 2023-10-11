81bf726609f570efeb580ea9d35ce170e7f7d31f
Jonathan_Kiprono <kmutai315@gmail.com>
2023-10-11 16:12:29 +0300

    Add necessarry folders and files

---
+++ m-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header/index.js';
+import './style.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>
+);
